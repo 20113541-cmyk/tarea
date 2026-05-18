7c0cd4db6f176571a5396c8f4b3620a107d6874d
20113541-cmyk <20113541@clases.edu.sv>
2026-05-18 17:21:39 -0600

    Ejercicio 3
    Se piden dos numeros y luego se envian para mostrar un resultado (resta)

---
+++ guia_ejercicio_R3.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero1 = $_POST['numb1'];
+$numero2 = $_POST['numb2'];
+
+$resta = $numero1 - $numero2;
+
+echo "<h1> La resta de los numeros es: " . $resta;
+echo "<br><a href='guia_ejercicio3.html'>Volver</a>";
+
+?>

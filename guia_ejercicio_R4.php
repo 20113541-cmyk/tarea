21aead5c5f5a73d1069f71ff5f6b9a6bf6477af1
20113541-cmyk <20113541@clases.edu.sv>
2026-05-18 17:24:03 -0600

    Ejercicio 4
    Se piden dos numeros y luego se envian para mostrar un resultado (division)

---
+++ guia_ejercicio_R4.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero1 = $_POST['numbe1'];
+$numero2 = $_POST['numbe2'];
+
+$multiplicacion = $numero1 * $numero2;
+
+echo "<h1> La multiplicación de los numeros es: " . $multiplicacion;
+echo "<br><a href='guia_ejercicio4.html'>Volver</a>";
+
+?>

bd40c5e839142f1e5ca6bb178a0c4cbd7f6eba58
20113541-cmyk <20113541@clases.edu.sv>
2026-05-18 17:17:53 -0600

    Ejercicio 2
    Se piden dos numeros y luego se envian para mostrar un resultado (suma)

---
+++ guia_ejercicio_R2.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero1 = $_POST['num1'];
+$numero2 = $_POST['num2'];
+
+$suma = $numero1 + $numero2;
+
+echo "<h1>La suma de los dos numeros es: " . $suma;
+echo "<br><a href='guia_ejercicio2.html'>Volver</a>";
+
+?>

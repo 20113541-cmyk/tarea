546cbceb16dcf6e3304911b5d2b9eaa992c5482d
20113541-cmyk <20113541@clases.edu.sv>
2026-05-19 10:08:26 -0600

    Ejercicio 7
    Se piden dos numeros y en el archivo php se muestra cual es el mayor

---
+++ guia_ejercicio_R7.php
@@ -0,0 +1,13 @@
+<?php
+
+$numero1 = $_POST['numero1'];
+$numero2 = $_POST['numero2'];
+
+if ($numero1 < $numero2){
+    echo "el numero " . $numero1 . " es mayor";
+} else {
+    echo "el numero " . $numero1 . " es mayor";
+}
+echo "<br><a href='guia_ejercicio7.html'>Volver</a>";
+
+?>

f7f06bcc798c5fcc2bb89b30ece51621dfa223d1
20113541-cmyk <20113541@clases.edu.sv>
2026-05-19 09:29:21 -0600

    Ejercicio 5
    Se escribiran dos numero y luego se ara el proceso de una divion y se mostrara el resultado

---
+++ guia_ejercicio_R5.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero1 = $_POST['number1'];
+$numero2 = $_POST['number2'];
+
+$division = $numero1 / $numero2;
+
+echo "<h1> La división de los numeros es: " . $division;
+echo "<br><a href='guia_ejercicio5.html'>Volver</a>";
+
+?>

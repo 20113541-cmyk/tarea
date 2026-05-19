f405b585b7050ebcf1628a6b1bf15c5b71a1d8ca
20113541-cmyk <20113541@clases.edu.sv>
2026-05-19 10:03:46 -0600

    guia_ejercicio_R7.php

--- guia_ejercicio_R7.php
+++ guia_ejercicio_R7.php
@@ -4,7 +4,7 @@ $numero1 = $_POST['numero1'];
 $numero2 = $_POST['numero2'];
 
 if ($numero1 < $numero2){
-    echo "el numero " . $numero1 . " es mayor";
+    echo "el numero " . $numero1 . " es mayor ";
 } else {
     echo "el numero " . $numero2 . "es mayor";
 }

cec7c448fcbde9dbba8dcd43c1f89076d0ddca5b
20113541-cmyk <20113541@clases.edu.sv>
2026-05-19 10:31:53 -0600

    Ejercicio 13
    aqui piden la longitud del lado de un cuadro y en el archivo php se muestre el perimetro

---
+++ guia_ejercicio_R13.php
@@ -0,0 +1,10 @@
+<?php
+
+$num = $_POST['dato'];
+
+$perimetro = $num * 4;
+
+echo "<h1>El perimetro de cuadrado es de: " . $perimetro . "</h1>";
+echo "<br><a href='guia_ejercicio13.html'>Volver</a>";
+
+?>
